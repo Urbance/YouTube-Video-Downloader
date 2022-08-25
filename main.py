8364e58a022099bc2fc0f08cd394a3602705a569
Martin <schmidtmartin697@gmail.com>
2022-08-25 21:38:59 +0200

    Start dev branch

--- main.py
+++ main.py
@@ -1,12 +1,95 @@
 import json
 import os
 import tkinter as tk
-import tkinter.font as font
-from tkinter import W, Misc, filedialog, messagebox, ttk
-from turtle import bgcolor
-
+from tkinter import filedialog, messagebox, ttk
 import pytube
+import moviepy.editor as mpe
+import shutil
 
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        # window setup
+        self.resizable(False, False)
+        self.title("YouTube Video Downloader")
+
+        x = (self.winfo_screenwidth() - self.winfo_reqwidth()) / 2
+        y = (self.winfo_screenheight() - self.winfo_reqheight()) / 2
+        self.geometry("+%d+%d" % (x, y))
+
+
+class MainFrame(ttk.Frame):
+    def __init__(self, container, v):
+        super().__init__(container)
+
+        global e_youtubelink
+        global e_targetdirectory
+
+        # objects setup
+        l_output_folder = ttk.Label(self, text=translation['path_to_location'])
+        l_output_folder.grid(row=0, column=0, sticky=tk.W)
+
+        e_targetdirectory = ttk.Entry(self)
+        e_targetdirectory.insert(1, outputfolder)
+        e_targetdirectory.grid(row=1, column=0, ipadx=100)
+
+        b_browse = ttk.Button(self, text=translation['browse'], command=update_directory)
+        b_browse.grid(row=2, column=0, pady=10)
+
+        l_youtubelink = ttk.Label(self, text=translation['link_to_youtube_video'])
+        l_youtubelink.grid(row=3, column=0, sticky=tk.W)
+        e_youtubelink = ttk.Entry(self)
+        e_youtubelink.grid(row=4, column=0, ipadx=100)
+
+        b_open_outputfolder = ttk.Button(self, text=translation['output_folder'], command=open_outputfolder)
+        b_open_outputfolder.grid(row=5, column=0, sticky=tk.W, pady=10)
+
+        options = ('Video', "Audio")
+        options_var = tk.StringVar()
+        om_format = ttk.OptionMenu(self, options_var, options[0], *options)
+        om_format.grid(row=5, column=0, sticky=tk.E)
+
+        b_next_frame = ttk.Button(self, text="Next", command=lambda:change_frame_to_download_section_and_get_video())
+        b_next_frame.grid(row=6, column=0, sticky=tk.E, pady=10)
+
+        self.pack(fill="both", expand=1)
+class DownloadSection(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+
+        global video_title
+
+        l_video_title = ttk.Label(self, text="title" + video_title)
+        l_video_title.grid(row=1, column=0)
+
+        b_download = ttk.Button(self, text=translation['download'], command=download_process)
+        b_download.grid(row=3, column=0)
+
+        b_settings = tk.Button(self, text="⚙", bd=0, highlightthickness=0, command=settings_window)
+        b_settings.grid(row=4, column=0, ipadx=50)
+def change_frame_to_download_section_and_get_video():
+    if e_targetdirectory.get() == '':
+        messagebox.showerror('YouTube Video Downloader', translation['no_directory_path'])
+        return
+
+    if e_youtubelink.get() == '':
+        messagebox.showerror('YouTube Video Downloader', translation['invalid_youtube_link'])
+        return
+
+    global video_title
+    global video_thumbnail
+
+    youtubelink = e_youtubelink.get()
+    get_video = pytube.YouTube(youtubelink)
+    video_title = get_video.title
+    video_thumbnail = get_video.thumbnail_url
+
+    print(video_title)
+    frame2.pack(fill="both", expand=1)
+    frame.forget()
 
 def setup_config_file():
     global outputfolder
@@ -26,10 +109,9 @@ def setup_config_file():
     with open('config.json', 'w') as file:
         json.dump(config_values, file, indent=4)
 
-def create_lang_en_file():
-    if os.path.exists('lang_en.json'):
-        return
+def setup_language_files():
 
+    # create english language file
     values = {
         "download": "Download",
         "format_option_audio": "Audio",
@@ -39,23 +121,21 @@ def create_lang_en_file():
         "path_to_location": "Path to location",
         "browse": "Browse",
         "language": "Language",
-        "restart_program": "Please restart the application.",
+        "restart_program": "The application is restarted for the change to take effect",
         "window_settings_title": "YouTube Video Downloader - Settings",
         "file_already_exists": "This file is already existing.",
+        "no_directory_path": "Please enter a directory path.",
         "unknown_file_format": "Please enter a valid file format.",
         "download_successfully": "The Video \"%video_title%\" successfully downloaded at \"%video_output_path%\".",
         "invalid_youtube_link": "Please enter a valid youtube link.",
-        "settings_confirm": "Confirm"
+        "settings_confirm": "Confirm",
+        "credits": "Design \"Radiance\" by RedFantom"
     }
 
     with open('lang_en.json', 'w') as file:
         json.dump(values, file, indent=4)
 
-
-def create_lang_de_file():
-    if os.path.exists('lang_de.json'):
-        return
-
+    # create german language file
     values = {
         "download": "Herunterladen",
         "format_option_audio": "Audio",
@@ -65,18 +145,36 @@ def create_lang_de_file():
         "path_to_location": "Pfad zum Speicherort",
         "browse": "Durchsuchen",
         "language": "Sprache",
-        "restart_program": "Die Anwendung muss neugestartet werden, damit die Änderung sichtbar wird.",
+        "restart_program": "Die Anwendung wird neugestartet, damit die Änderung wirksam wird.",
         "window_settings_title": "YouTube Video Downloader - Einstellungen",
         "file_already_exists": "Die Datei existiert bereits.",
+        "no_directory_path": "Bitte gebe einen Ausgabepfad an.",
         "unknown_file_format": "Bitte gebe ein gültiges Format an.",
         "download_successfully": "Das Video \"%video_title%\" wurde unter dem Pfad \"%video_output_path%\" gespeichert.",
         "invalid_youtube_link": "Bitte gebe einen gültigen YouTube-Link an.",
-        "settings_confirm": "Bestätigen"
+        "settings_confirm": "Bestätigen",
+        "credits": "Design \"Radiance\" by RedFantom"
     }
 
     with open('lang_de.json', 'w') as file:
         json.dump(values, file, indent=4)
 
+def setup_program_language():
+    global translation
+    global config
+
+    with open('config.json', 'r') as config_file:
+        config = json.load(config_file)
+
+    language = config['language']
+
+    if language == 'Deutsch':
+        with open('lang_de.json', 'r') as t_file:
+            translation = json.load(t_file)
+
+    if language == 'English':
+        with open('lang_en.json', 'r') as t_file:
+            translation = json.load(t_file)
 
 def update_directory():
     global outputfolder
@@ -93,7 +191,7 @@ def update_directory():
     with open('config.json', 'w') as file:
         json.dump(data, file, indent=4)
 
-def set_language():
+def fill_language_field():
     global c_language
 
     match config['language']:
@@ -103,61 +201,24 @@ def set_language():
             c_language.current(1)
 
 def update_language(event):
-    language = c_language.get()
-
-    with open('config.json', 'w') as file:
-        config['language'] = language
-        json.dump(config, file, indent=4)
-
-    messagebox.showinfo("YouTube Video Downloader", translation['restart_program'])
+    pass
+    # selected_language = c_language.get()
+    #
+    # with open('config.json', 'w') as file:
+    #     config['language'] = selected_language
+    #     json.dump(config, file, indent=4)
+    #
+    # messagebox.showinfo("YouTube Video Downloader", translation['restart_program'])
+    # root.destroy()
+    # os.startfile("main.py")
 
 def open_outputfolder():
     os.startfile(outputfolder)
 
 def close_settings_window():
     settings.destroy()
-    root.deiconify()
-
-def settings_window():
-    # global e_targetdirectory
-    global c_language
-    global settings
-
-    # vanish root window
-    root.withdraw()
-
-    # window setup
-    settings = tk.Toplevel()
-    settings.title(translation['window_settings_title'])
-    settings.resizable(False, False)
-
-    # setup objects
-    label2 = ttk.Label(settings, text=translation['path_to_location'])
-    label2.grid(row=0, column=0)
-
-    e_targetdirectory = ttk.Entry(settings)
-    e_targetdirectory.insert(1, outputfolder)
-    e_targetdirectory.grid(row=1, column=0, ipadx=20)
-
-    b_browse = ttk.Button(settings, text=translation['browse'], command=update_directory)
-    b_browse.grid(row=2, column=0)
-
-    l_language = ttk.Label(settings, text=translation['language'])
-    l_language.grid(row=3, column=0)
-
-    c_language = ttk.Combobox(settings, values=["English", "Deutsch"], state="readonly")
-    set_language()
-    c_language.grid(row=4, column=0)
-    c_language.bind('<<ComboboxSelected>>', update_language)
-    
-    b_closewindow = ttk.Button(settings, text=translation['settings_confirm'], command=close_settings_window)
-    b_closewindow.grid(row=5, column=0)
-
+    app.deiconify()
 def download_process():
-    if e_youtubelink.get() == '':
-        messagebox.showerror('YouTube Video Downloader', translation['invalid_youtube_link'])
-        return
-
     format_value = options_var.get()
     youtubelink = e_youtubelink.get()
     get_video = pytube.YouTube(youtubelink)
@@ -166,7 +227,32 @@ def download_process():
     try:
         match format_value:
             case "Video":
-                get_video.streams.filter(file_extension='mp4').get_highest_resolution().download(outputfolder)
+                vname = "video.mp4"
+                aname = "audio.mp3"
+
+                # download video and audio and rename both files
+                yt_video = get_video.streams.filter(mime_type="video/mp4", progressive=False).order_by("resolution").desc().first().download()
+                print(get_video.streams.filter(mime_type="video/mp4", progressive=False).order_by("resolution").desc().first().resolution)
+                os.rename(yt_video, vname)
+                audio = get_video.streams.filter(only_audio=True).first().download()
+                os.rename(audio, aname)
+
+                video = mpe.VideoFileClip(vname)
+                audio = mpe.AudioFileClip(aname)
+
+                final = video.set_audio(audio)
+                final.write_videofile("final.mp4")
+
+                video.close()
+                audio.close()
+                final.close()
+
+                # clean up
+                os.remove(vname)
+                os.remove(aname)
+
+                os.rename("final.mp4", yt_video)
+                shutil.move(yt_video, outputfolder)
             case "Audio":
                 get_video = get_video.streams.filter(only_audio=True).first().download(outputfolder)
                 file, ext = os.path.splitext(get_video)
@@ -183,68 +269,60 @@ def download_process():
     successfully_downloaded = successfully_downloaded.replace('%video_output_path%', outputfolder)
     messagebox.showinfo("YouTube Video Downloader", successfully_downloaded)
 
+def settings_window():
+    global c_language
+    global settings
+
+    # vanish root window
+    app.withdraw()
+
+    # window setup
+    settings = tk.Toplevel()
+    settings.title(translation['window_settings_title'])
+    settings.resizable(False, False)
+
+    # setup objects
+    l_language = ttk.Label(settings, text=translation['language'])
+    l_language.grid(row=1, column=0, sticky="W")
+
+    c_language = ttk.Combobox(settings, values=["English", "Deutsch"], state="readonly")
+    fill_language_field()
+    c_language.grid(row=2, column=0, ipadx=100)
+    c_language.bind('<<ComboboxSelected>>', update_language)
+
+    b_closewindow = ttk.Button(settings, text=translation['settings_confirm'], command=close_settings_window)
+    b_closewindow.grid(row=3, column=0, pady=10)
+
+    l_credits = ttk.Label(settings, text=translation['credits'])
+    l_credits.grid(row=4, column=0, sticky="W")
+
 def main_window():
     global options_var
     global e_youtubelink
     global e_targetdirectory
-
-    options = ('Video', "Audio")
-    options_var = tk.StringVar()
-
-    # window setup
-    root.resizable(False, False)
-    root.title("YouTube Video Downloader")
-    root.geometry("+300+300")
-
-    # setup and set layouts
-    l_output_folder = tk.Label(text=translation['path_to_location'], font=("Arial", 10))
-    l_output_folder.grid(row=0, column=0, sticky=tk.W, padx=10)
-
-    e_targetdirectory = tk.Entry(font=("Arial", 12), borderwidth=0)
-    e_targetdirectory.insert(1, outputfolder)
-    e_targetdirectory.grid(row=1, column=0, ipadx=100, ipady=5, padx=10)
-
-    # TODO Button doesn't work, only in settings!
-    b_browse = tk.Button(text=translation['browse'], command=update_directory, font=("Arial", 10), background="#83838B", foreground="white", relief="flat")
-    b_browse.grid(row=2, column=0, pady=10, ipadx=50, ipady=5)
-
-    l_youtubelink = tk.Label(text=translation['link_to_youtube_video'], font=("Arial", 10))
-    l_youtubelink.grid(row=3, column=0, sticky=tk.W, padx=10)
-    e_youtubelink = tk.Entry(font=("Arial", 12), borderwidth=0)
-    e_youtubelink.grid(row=4, column=0, ipadx=100, ipady=5, padx=10)
-
-    b_open_outputfolder = tk.Button(text=translation['output_folder'], command=open_outputfolder, font=("Arial", 10), background="#83838B", foreground="white", relief="flat")
-    b_open_outputfolder.grid(row=5, column=0, sticky=tk.W, pady=10, padx=5 , ipadx=50, ipady=5)
-
-    om_format = ttk.OptionMenu(root, options_var, options[0], *options)
-    om_format.grid(row=5, column=0, sticky=tk.E)
-
-    b_download = tk.Button(text=translation['download'], command=download_process, font=("Arial", 10), background="#27a102", foreground="white", relief="flat")
-    b_download.grid(row=6, column=0, padx=10, ipadx=50, ipady=5)
-
-    b_settings = tk.Button(text="⚙️", bd=0, highlightthickness=0, command=settings_window)
-    b_settings.grid(row=7, column=0, ipadx=50)
+    global test_frame
 
     # Query object
     # print(Misc.winfo_class(e_targetdirectory))
-root = tk.Tk()
 
-setup_config_file()
-create_lang_en_file()
-create_lang_de_file()
 
-with open('config.json', 'r') as config_file:
-    config = json.load(config_file)
+    main_window()
 
-language = config['language']
 
-if language == 'Deutsch':
-    with open('lang_de.json', 'r') as t_file:
-        translation = json.load(t_file)
+if __name__ == "__main__":
+    # startup
+    setup_config_file()
+    setup_language_files()
+    setup_program_language()
 
-if language == 'English':
-    with open('lang_en.json', 'r') as t_file:
-        translation = json.load(t_file)
+    # setup window and frame
+    app = App()
+    frame = MainFrame(app)
+    frame2 = DownloadSection(app)
 
-main_window()
-root.mainloop()
+    # setup style
+    style = ttk.Style()
+    app.tk.call('source', 'themes/radiance/radiance.tcl')
+    style.theme_use('radiance')
+
+    app.mainloop()
