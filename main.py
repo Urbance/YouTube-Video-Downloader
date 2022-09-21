409b0e640e970462142fdc93ffae366f641107b2
Martin <schmidtmartin697@gmail.com>
2022-09-21 17:32:22 +0200

    fix: Rebuild GUI, detach FinalFrame and ConfirmationFrame

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 """
 YouTube-Video-Downloader
+A simple YouTube Video Downloader that supports .mp3 and .mp4 format
 """
 
 import json
 import os
+import threading
+from io import BytesIO
+
 import pytube
+import requests
+from PIL import ImageTk, Image
 from pytube import Playlist
 import moviepy.editor as mpe
 import shutil
@@ -22,6 +28,7 @@ class App(Tk):
         x = (self.winfo_screenwidth() - self.winfo_reqwidth()) / 2
         y = (self.winfo_screenheight() - self.winfo_reqheight()) / 2
         self.geometry("+%d+%d" % (x, y))
+
 
 class MainFrame(ttk.Frame):
     def __init__(self, container):
@@ -55,7 +62,7 @@ class MainFrame(ttk.Frame):
         om_format = ttk.OptionMenu(self, options_var, options[0], *options)
         om_format.grid(row=5, column=0, sticky=E)
 
-        b_next_frame = ttk.Button(self, text=translation['next'], command=lambda : self.change_window())
+        b_next_frame = ttk.Button(self, text=translation['download'], command=lambda: self.change_to_download_frame())
         b_next_frame.grid(row=6, column=0, sticky=E, pady=10)
 
         b_settings = Button(self, text="⚙", bd=0, highlightthickness=0, command=settings_window)
@@ -63,8 +70,7 @@ class MainFrame(ttk.Frame):
 
         self.pack(fill="both", expand=1)
 
-    def change_window(self):
-        global download_section_frame
+    def change_to_download_frame(self):
         global video_title
 
         if e_targetdirectory.get() == '':
@@ -83,47 +89,72 @@ class MainFrame(ttk.Frame):
             playlist = Playlist(youtubelink)
             video_title = playlist.title
 
-        download_section_frame = DownloadSectionFrame(app)
-        download_section_frame.pack(fill="both", expand=1)
-        frame.forget()
+        download_frame = DownloadFrame(app)
+        download_frame.pack(fill="both", expand=1)
+        self.forget()
 
-class DownloadSectionFrame(ttk.Frame):
+
+class DownloadFrame(ttk.Frame):
     def __init__(self, container):
         super().__init__(container)
 
-        format_value = options_var.get()
+        global l_state_line
+        global pb
 
-        l_video_informations = ttk.Label(self, text=translation['video_title'] + ": " + video_title + "\n" + translation['video_format'] + ": " + format_value + "\n" + translation['video_resolution'] + ": " + "Highest Resolution")
-        l_video_informations.grid(row=1, column=0)
+        video_link = e_youtubelink.get()
+        video_format = options_var.get()
 
-        b_back = ttk.Button(self, text=translation['back'], command=lambda:self.change_to_main_frame())
-        b_back.grid(row=3, column=0, sticky=W)
+        l_video_title = ttk.Label(
+            self, text=video_title)
+        # TODO add video resolution
+        l_video_title.grid(row=1, column=0)
+        l_video_information = ttk.Label(self, text=f"{video_format}")
+        l_video_information.grid(row=2, column=0)
+        # setup thumbnail
+        thumbnail_url = pytube.YouTube(video_link).thumbnail_url
+        response = requests.get(thumbnail_url)
+        thumbnail = Image.open(BytesIO(response.content))
+        thumbnail = thumbnail.resize((225, 150), Image.ANTIALIAS)
+        thumbnail = ImageTk.PhotoImage(thumbnail)
+        l_thumbnail = ttk.Label(self, image=thumbnail)
+        l_thumbnail.image = thumbnail
+        l_thumbnail.grid(row=3, pady=5)
+        # setup progressbar
+        pb = ttk.Progressbar(
+            self,
+            orient='horizontal',
+            mode='indeterminate',
+            length=280
+        )
+        pb.grid(row=4, column=0)
+        pb.start(20)
+        l_state_line = ttk.Label(
+            self, text=" "
+        )
+        l_state_line.grid(row=5, column=0)
 
-        b_download = ttk.Button(self, text=translation['download'], command=lambda:self.download_process())
-        b_download.grid(row=3, column=1, sticky=E)
+        self.download_setup()
 
-    def change_to_main_frame(self):
-        self.destroy()
+    def download_setup(self):
+        # create thread
+        new_thread = threading.Thread(target=lambda: self.download_video(), daemon=True)
+        new_thread.start()
 
-        frame.pack(fill="both", expand=1)
-
-    def download_process(self):
-        global video_title
-        global final_frame
-
-        format_value = options_var.get()
-        youtubelink = e_youtubelink.get()
+    def download_video(self):
         temp_download_directory = "temp_download"
-
         if os.path.exists(temp_download_directory):
             shutil.rmtree(temp_download_directory)
 
+        video_format = options_var.get()
+        video_link = e_youtubelink.get()
+        video_title = video_link.title()
+
         try:
-            match format_value:
+            match video_format:
                 case "Video":
                     vname = "video.mp4"
                     aname = "audio.mp3"
-                    get_video = pytube.YouTube(youtubelink)
+                    get_video = pytube.YouTube(video_link)
                     video_title = get_video.title
 
                     # setup download
@@ -132,7 +163,7 @@ class DownloadSectionFrame(ttk.Frame):
                     # download video and audio and rename both files and move final file
                     print(f"Start downloading \"{video_title}\"")
                     print("Download video file")
-
+                    l_state_line.config(text="Download video file")
                     # TODO .download paramter file_name
                     yt_video = get_video.streams.filter(mime_type="video/mp4", progressive=False).order_by(
                         "resolution").desc().first().download(temp_download_directory)
@@ -140,11 +171,13 @@ class DownloadSectionFrame(ttk.Frame):
                     os.rename(yt_video, temp_video_file)
 
                     print("Download audio file")
+                    l_state_line.config(text="Download audio file")
                     audio = get_video.streams.filter(only_audio=True).first().download(temp_download_directory)
                     temp_audio_file = os.path.join(temp_download_directory, aname)
                     os.rename(audio, temp_audio_file)
 
                     print("Merge audio and video file ")
+                    l_state_line.config(text="Merge audio and video file")
                     video = mpe.VideoFileClip(temp_video_file)
                     audio = mpe.AudioFileClip(temp_audio_file)
 
@@ -166,7 +199,7 @@ class DownloadSectionFrame(ttk.Frame):
                     os.mkdir(temp_download_directory)
 
                     # download video
-                    get_video = pytube.YouTube(youtubelink)
+                    get_video = pytube.YouTube(video_link)
                     video_title = get_video.title
                     video = get_video.streams.filter(only_audio=True).first().download(temp_download_directory)
 
@@ -185,7 +218,7 @@ class DownloadSectionFrame(ttk.Frame):
                     os.removedirs(temp_download_directory)
 
                 case "Playlist Audio":
-                    playlist = Playlist(youtubelink)
+                    playlist = Playlist(video_link)
                     video_title = playlist.title
 
                     for video in playlist.videos:
@@ -203,33 +236,28 @@ class DownloadSectionFrame(ttk.Frame):
             messagebox.showerror("YouTube Video Downloader", translation['file_already_exists'])
             return
 
-        self.change_to_final_frame()
 
-    def change_to_final_frame(self):
-        final_frame = FinalFrame(app)
-        final_frame.pack(fill="both", expand=1)
-        self.forget()
-
-class FinalFrame(ttk.Frame):
-    def __init__(self, container):
-        super().__init__(container)
-
+        # MMS Start detach FinalFrame and ConfirmationFrame
+        pb.destroy()
         successfully_downloaded = translation['download_successfully']
         successfully_downloaded = successfully_downloaded.replace('%video_title%', video_title)
-        successfully_downloaded = successfully_downloaded.replace('%video_output_path%', outputfolder)
-        l_successfully_downloaded = ttk.Label(self, text=successfully_downloaded)
-        l_successfully_downloaded.grid(row=1, column=0)
+        l_state_line.config(text=successfully_downloaded)
+        b_download_new_video = ttk.Button(
+            self, text=translation['download_new_video'], command=lambda: self.change_to_main_frame())
+        b_download_new_video.grid(
+            row=6, column=0, sticky=E)
+        b_open_outputfolder = ttk.Button(
+            self, text=translation['output_folder'], command=open_outputfolder)
+        b_open_outputfolder.grid(
+            row=6, column=0, sticky=W, pady=10)
+        # MMS End detach final frame
 
-        b_download_new_video = ttk.Button(self, text=translation['download_new_video'], command=lambda:self.change_to_main_frame())
-        b_download_new_video.grid(row=2, column=0, sticky=E)
-
-        b_open_outputfolder = ttk.Button(self, text=translation['output_folder'], command=open_outputfolder)
-        b_open_outputfolder.grid(row=2, column=0, sticky=W, pady=10)
 
     def change_to_main_frame(self):
         e_youtubelink.delete(0, "end")
         self.forget()
         frame.pack(fill="both", expand=1)
+
 
 def settings_window():
     global c_language
@@ -262,6 +290,7 @@ def settings_window():
     l_credits = ttk.Label(settings, text=translation['credits'])
     l_credits.grid(row=4, column=0, sticky="W")
 
+
 def setup_config_file():
     global outputfolder
 
@@ -286,15 +315,15 @@ def setup_config_file():
     with open('config.json', 'w') as file:
         json.dump(config_values, file, indent=4)
 
+
 def setup_language_files():
-    # create english language file
     values = {
         "download": "Download »",
         "format_option_audio": "Audio",
         "format_option_video": "Video",
         "link_to_youtube_video": "Link to YouTube-Video",
         "output_folder": "Open output-folder",
-        "path_to_location": "Path to location",
+        "path_to_location": "Path to output-folder",
         "browse": "Browse",
         "language": "Language",
         "restart_program": "The application is restarted for the change to take effect",
@@ -302,17 +331,13 @@ def setup_language_files():
         "file_already_exists": "This file is already existing.",
         "no_directory_path": "Please enter a directory path.",
         "unknown_file_format": "Please enter a valid file format.",
-        "download_successfully": "The Video \"%video_title%\"\nsuccessfully downloaded at \"%video_output_path%\".",
+        "download_successfully": "The Video \"%video_title%\"\n was successfully downloaded",
         "invalid_youtube_link": "Please enter a valid youtube link.",
         "settings_confirm": "Confirm",
         "credits": "Design \"Radiance\" by RedFantom",
-        "next": "Next »",
-        "back": "« Back",
-        "video_title": "Title",
-        "video_format": "Format",
-        "video_resolution": "Resolution",
         "download_new_video": "Download new video"
     }
+    # create english language file
 
     with open('lang_en.json', 'w') as file:
         json.dump(values, file, indent=4)
@@ -332,20 +357,16 @@ def setup_language_files():
         "file_already_exists": "Die Datei existiert bereits.",
         "no_directory_path": "Bitte gebe einen Ausgabepfad an.",
         "unknown_file_format": "Bitte gebe ein gültiges Format an.",
-        "download_successfully": "Das Video \"%video_title%\"\nwurde unter dem Pfad \"%video_output_path%\" gespeichert.",
+        "download_successfully": "Das Video \"%video_title%\"\nwurde erfolgreich heruntergeladen",
         "invalid_youtube_link": "Bitte gebe einen gültigen YouTube-Link an.",
         "settings_confirm": "Bestätigen",
         "credits": "Design \"Radiance\" by RedFantom",
-        "next": "Weiter »",
-        "back": "« Zurück",
-        "video_title": "Titel",
-        "video_format": "Format",
-        "video_resolution": "Auflösung",
         "download_new_video": "Neues Video herunterladen"
     }
 
     with open('lang_de.json', 'w') as file:
         json.dump(values, file, indent=4)
+
 
 def setup_program_language():
     global translation
@@ -364,6 +385,7 @@ def setup_program_language():
         with open('lang_en.json', 'r') as t_file:
             translation = json.load(t_file)
 
+
 def update_directory():
     global outputfolder
 
@@ -379,6 +401,7 @@ def update_directory():
     with open('config.json', 'w') as file:
         json.dump(data, file, indent=4)
 
+
 def fill_language_field():
     global c_language
 
@@ -387,6 +410,7 @@ def fill_language_field():
             c_language.current(0)
         case 'Deutsch':
             c_language.current(1)
+
 
 def update_language(event):
     selected_language = c_language.get()
@@ -399,12 +423,15 @@ def update_language(event):
     app.destroy()
     os.startfile("main.py")
 
+
 def open_outputfolder():
     try:
         os.startfile(outputfolder)
     except FileNotFoundError:
         os.mkdir(outputfolder)
         os.startfile(outputfolder)
+
+
 def close_settings_window():
     settings.destroy()
     app.deiconify()
@@ -422,8 +449,8 @@ if __name__ == "__main__":
 
     # setup style
     style = ttk.Style()
-    app.tk.call('source', 'themes/radiance/radiance.tcl')
-    style.theme_use('radiance')
+    app.tk.call('source', 'themes/azure/azure.tcl')
+    app.tk.call('set_theme', 'dark')
 
     app.mainloop()
 
